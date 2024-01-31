a62a7c08c2658236a3edfa0685074ce107f6df02
Arkzeil <75022292+Arkzeil@users.noreply.github.com>
2024-01-31 20:14:39 +0800

    fixed path

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -4,7 +4,7 @@ target=aesdsocket
 
 case $1 in
     #start aesdsocket in daemon mode
-    start) ./$target -d
+    start) $target -d
         ;;
     # send a SIGTERM signal(default is 15 actually) to aesdsocket child process
     stop) pkill -15 $target
